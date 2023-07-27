9ce3bb76019a0adff0233e65b54eefbd9ddf9cf8
Rushi Agrawal <rushi.agr@gmail.com>
2023-07-27 12:10:59 +0530

    allow 'debug' param into Zerodha() object

--- jugaad_trader/zerodha.py
+++ jugaad_trader/zerodha.py
@@ -70,12 +70,12 @@ class Zerodha(KiteConnect):
 
     _default_root_uri = "https://kite.zerodha.com"
 
-    def __init__(self, user_id=None, password=None, twofa=None):
+    def __init__(self, user_id=None, password=None, twofa=None, debug=False):
         self.user_id = user_id
         self.password = password
         self.twofa = twofa
 
-        super().__init__(api_key="")
+        super().__init__(api_key="", debug=debug)
         self.s = self.reqsession = requests.Session()
         headers = {
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36",
